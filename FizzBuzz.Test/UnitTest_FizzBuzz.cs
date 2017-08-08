b71a7afd1dd50ad4a870115bd5518fa11b8148cf
Martin Smith <smithdev1237@gmail.com>
2017-08-08 13:51:58 +0100

    first commit

---
+++ FizzBuzz.Test/UnitTest_FizzBuzz.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Test
+{
+    [TestClass]
+    public class UnitTest_FizzBuzz
+    {
+        [TestMethod]
+        public void Can_Find_Fizz()
+        {
+            Assert.AreEqual("Fizz", FizzBuzz.Solve(3));
+        }
+
+        [TestMethod]
+        public void Can_Find_Buzz()
+        {
+            Assert.AreEqual("Buzz", FizzBuzz.Solve(5));
+        }
+
+        [TestMethod]
+        public void Can_Find_FizzBuzz()
+        {
+            Assert.AreEqual("Fizz Buzz", FizzBuzz.Solve(15));
+        }
+
+        [TestMethod]
+        public void Outputs_Number_If_Not_Fizz_Or_Buzz()
+        {
+            Assert.AreEqual("1", FizzBuzz.Solve(1));
+        }
+
+        [TestMethod]
+        public void Can_Handle_Zero_As_Input()
+        {
+            Assert.AreEqual("Fizz Buzz", FizzBuzz.Solve(0));
+        }
+
+        [TestMethod]
+        public void Can_Solve_1_To_100()
+        {
+            var data = FizzBuzz.SolveForRange(1, 100);
+            Assert.AreEqual(100, data.Count);
+            Assert.AreEqual("Fizz", data[3 - 1]);
+            Assert.AreEqual("Buzz", data[5 - 1]);
+            Assert.AreEqual("Fizz Buzz", data[15 - 1]);
+            Assert.AreEqual("1", data[1 - 1]);            
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzz
+    {
+        public static string Solve(int number)
+        {
+            //if(number < 1)
+            //{
+            //    return null;
+            //}
+
+            bool fizz = number % 3 == 0;
+            bool buzz = number % 5 == 0;
+
+            if (fizz && buzz)
+                return "Fizz Buzz";
+            else if (fizz)
+                return "Fizz";
+            else if (buzz)
+                return "Buzz";
+            else
+                return number.ToString();
+        }
+
+        public static List<string> SolveForRange(int start, int end)
+        {
+            var result = new List<string>();
+
+            for(var i = start; i <= end; i++)
+            {
+                result.Add(Solve(i));
+            }
+
+            return result;
+        }
+    }
+}
